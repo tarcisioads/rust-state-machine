7bcac43dde9098372a029fb5d0e8d96afa84b15b
Tarcisio Ads <tarcisioasd@gmail.com>
2024-08-21 14:54:03 -0300

    add dispatch to proof of existence

--- src/main.rs
+++ src/main.rs
@@ -15,16 +15,19 @@ mod types {
     pub type Extrinsic = support::Extrinsic<AccountId, crate::RuntimeCall>;
     pub type Header = support::Header<BlockNumber>;
     pub type Block = support::Block<Header, Extrinsic>;
+    pub type Content = String;
 }
 
 pub enum RuntimeCall {
     Balances(balances::Call<Runtime>),
+    ProofOfExistence(proof_of_existence::Call<Runtime>),
 }
 
 #[derive(Debug)]
 pub struct Runtime {
     system: system::Pallet<Self>,
     balances: balances::Pallet<Self>,
+    proof_of_existence: proof_of_existence::Pallet<Self>,
 }
 
 impl system::Config for Runtime {
@@ -37,6 +40,10 @@ impl balances::Config for Runtime {
     type Balance = types::Balance;
 }
 
+impl proof_of_existence::Config for Runtime {
+    type Content = types::Content;
+}
+
 impl crate::support::Dispatch for Runtime {
     type Caller = <Runtime as system::Config>::AccountId;
     type Call = RuntimeCall;
@@ -44,6 +51,7 @@ impl crate::support::Dispatch for Runtime {
     fn dispatch(&mut self, caller: Self::Caller, call: Self::Call) -> support::DispatchResult {
         match call {
             RuntimeCall::Balances(call) => self.balances.dispatch(caller, call),
+            RuntimeCall::ProofOfExistence(call) => self.proof_of_existence.dispatch(caller, call),
         }
     }
 }
@@ -53,6 +61,7 @@ impl Runtime {
         Self {
             system: system::Pallet::new(),
             balances: balances::Pallet::new(),
+            proof_of_existence: proof_of_existence::Pallet::new(),
         }
     }
 
@@ -90,14 +99,14 @@ fn main() {
             support::Extrinsic {
                 caller: alice.clone(),
                 call: RuntimeCall::Balances(balances::Call::Transfer {
-                    to: bob,
+                    to: bob.clone(),
                     amount: 30,
                 }),
             },
             support::Extrinsic {
-                caller: alice,
+                caller: alice.clone(),
                 call: RuntimeCall::Balances(balances::Call::Transfer {
-                    to: charlie,
+                    to: charlie.clone(),
                     amount: 20,
                 }),
             },
@@ -105,6 +114,33 @@ fn main() {
     };
 
     runtime.execute_block(block).expect("invalid block");
+
+    let block_1 = types::Block {
+        header: support::Header { block_number: 2 },
+        extrinsics: vec![
+            support::Extrinsic {
+                caller: alice.clone(),
+                call: RuntimeCall::ProofOfExistence(proof_of_existence::Call::CreateClaim { 
+                    claim: "My Document".to_string(),
+                }),
+            },
+            support::Extrinsic {
+                caller: bob.clone(),
+                call: RuntimeCall::ProofOfExistence(proof_of_existence::Call::CreateClaim { 
+                    claim: "My Document 2".to_string(),
+                }),
+            },
+            support::Extrinsic {
+                caller: alice.clone(),
+                call: RuntimeCall::ProofOfExistence(proof_of_existence::Call::RevokeClaim { 
+                    claim: "My Document".to_string(),
+                }),
+            },
+        ],
+    };
+
+    runtime.execute_block(block_1).expect("invalid block");
+
 
     println!("{:#?}", runtime);
 }
--- src/proof_of_existence.rs
+++ src/proof_of_existence.rs
@@ -1,14 +1,45 @@
 use core::fmt::Debug;
 use std::collections::BTreeMap;
+use crate::support::DispatchResult;
 
 pub trait Config: crate::system::Config {
     type Content: Debug + Ord;
 }
 
+pub enum Call<T: Config> {
+    CreateClaim { claim: T::Content},
+    RevokeClaim { claim: T::Content},
+}
+
+
 #[derive(Debug)]
 pub struct Pallet<T: Config> {
     claims: BTreeMap<T::Content, T::AccountId>
 }
+
+impl<T: Config> crate::support::Dispatch for Pallet<T> {
+    type Caller = T::AccountId;
+    type Call = Call<T>;
+
+    fn dispatch(
+        &mut self,
+        caller: Self::Caller,
+        call: Self::Call
+    ) -> crate::support::DispatchResult{
+        match call {
+            Call::CreateClaim { claim } => {
+                self.create_claim(caller, claim)?
+            }
+            Call::RevokeClaim { claim } => {
+                self.revoke_claim(caller, claim)?
+            }
+        }
+        Ok(())
+    }
+
+
+}
+
 
 impl<T: Config> Pallet<T> {
     pub fn new() -> Self {
@@ -16,5 +47,82 @@ impl<T: Config> Pallet<T> {
             claims: BTreeMap::new(),
         }
     }
+
+    pub fn get_claim(&self, claim: &T::Content) -> Option<&T::AccountId> {
+        self.claims.get(claim)
+    }
+
+    pub fn create_claim(&mut self, caller: T::AccountId, claim: T::Content) -> DispatchResult {
+        if self.claims.contains_key(&claim) {
+            return Err(&"this content is already claimed");
+        }
+        self.claims.insert(claim, caller);
+        Ok(())
+    }
+
+    pub fn revoke_claim(&mut self, caller: T::AccountId, claim: T::Content) -> DispatchResult {
+        let owner = self.claims.get(&claim).ok_or("no claim found")?;
+        if owner != &caller {
+            return Err(&"this claim is owned by someone else");
+        }
+        self.claims.remove(&claim);
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::proof_of_existence::Pallet;
+
+    struct TestConfig;
+
+    impl super::Config for TestConfig {
+        type Content = &'static str;
+    }
+
+    impl crate::system::Config for TestConfig {
+        type AccountId = &'static str;
+        type BlockNumber = u32;
+        type Nonce = u32;
+    }
+
+    #[test]
+    fn basic_proof_of_existence(){
+        let alice = "alice";
+        let bob = "bob";
+        let mut poe = Pallet::<TestConfig>::new();
+        assert!(poe.get_claim(&"Hello").is_none());
+
+        let _ = poe.create_claim(alice, "Hello");
+
+        assert!(poe.create_claim(bob, "Hello").is_err())
+
+    }
+
+    #[test]
+    fn revoke(){
+        let alice = "alice";
+        let bob = "bob";
+        let mut poe = Pallet::<TestConfig>::new();
+        let _ = poe.create_claim(alice, "Hello");
+        
+        assert!(poe.revoke_claim(bob, "Hello").is_err());
+        assert!(poe.revoke_claim(alice, "Hello").is_ok());
+        assert!(poe.get_claim(&"Hello").is_none());
+
+        assert!(poe.revoke_claim(bob, "No Claim Exist").is_err());
+    }
+
+    #[test]
+    fn cant_claim_existing_claim(){
+        let alice = "alice";
+        let mut poe = Pallet::<TestConfig>::new();
+        assert!(poe.get_claim(&"Hello").is_none());
+
+        let _ = poe.create_claim(alice, "Hello");
+
+        assert_eq!(poe.get_claim(&"Hello"), Some(&alice));
+    }
+
 }
 
